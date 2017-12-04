74cc4558ead02e54ab30643c3b33a555245e454d
Davide Giovannini <giovannini.davide90@gmail.com>
2017-12-04 21:12:22 +0100

    added changes to support latest sdl2 mixer

--- src/sdl2_utils.rs
+++ src/sdl2_utils.rs
@@ -1,8 +1,6 @@
 use sdl2;
 use sdl2::image::INIT_PNG; // INIT_JPG
 
-use sdl2::mixer::INIT_OGG;
-
 use opengl;
 use gl;
 
@@ -10,12 +8,13 @@ use engine::make_engine;
 use Engine;
 
 #[inline]
-pub fn initialize_engine(window_title: &str,
-                         width: u32,
-                         height: u32,
-                         fullscreen: bool,
-                         logical_size: Option<(u32, u32)>)
-                         -> Engine {
+pub fn initialize_engine(
+    window_title: &str,
+    width: u32,
+    height: u32,
+    fullscreen: bool,
+    logical_size: Option<(u32, u32)>,
+) -> Engine {
     let sdl_context = sdl2::init().unwrap();
     let _image_context = sdl2::image::init(INIT_PNG).unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -34,7 +33,8 @@ pub fn initialize_engine(window_title: &str,
 
     let window = window_builder.build().unwrap();
 
-    let mut renderer = window.into_canvas()
+    let mut renderer = window
+        .into_canvas()
         .accelerated()
         .index(opengl::find_sdl_gl_driver().unwrap())
         .target_texture()
@@ -44,22 +44,21 @@ pub fn initialize_engine(window_title: &str,
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
     // Set the context into debug mode
-    #[cfg(debug_assertions)]
-    gl_attr.set_context_flags().debug().set();
+    #[cfg(debug_assertions)] gl_attr.set_context_flags().debug().set();
     // Set the OpenGL context version (OpenGL 3.1)
     gl_attr.set_context_version(3, 1);
 
 
     gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
-    renderer.window()
-        .gl_set_context_to_current()
-        .unwrap();
+    renderer.window().gl_set_context_to_current().unwrap();
 
     #[cfg(debug_assertions)]
-    println!("Opengl {}.{} version {:#?}",
-             gl_attr.context_major_version(),
-             gl_attr.context_minor_version(),
-             gl_attr.context_profile());
+    println!(
+        "Opengl {}.{} version {:#?}",
+        gl_attr.context_major_version(),
+        gl_attr.context_minor_version(),
+        gl_attr.context_profile()
+    );
 
     if let Some((width, height)) = logical_size {
         renderer.set_logical_size(width, height).unwrap();
@@ -69,7 +68,13 @@ pub fn initialize_engine(window_title: &str,
 
     let texture_creator = renderer.texture_creator();
 
-    make_engine(renderer, texture_creator, ttf_context, timer_subsystem, event_pump)
+    make_engine(
+        renderer,
+        texture_creator,
+        ttf_context,
+        timer_subsystem,
+        event_pump,
+    )
 }
 
 
@@ -78,22 +83,22 @@ pub fn initialize_engine(window_title: &str,
 
 
 #[inline]
-fn init_sdl_mixer() -> sdl2::mixer::Sdl2MixerContext {
-    let _mixer_context = sdl2::mixer::init(INIT_OGG).unwrap();
+fn init_sdl_mixer() {
     sdl2::mixer::open_audio(44_100, sdl2::mixer::AUDIO_S16LSB, 2, 1024).unwrap();
     sdl2::mixer::allocate_channels(32);
-    _mixer_context
 }
 
 
 pub fn log_system_info() -> String {
-    format!(r#"System info:
+    format!(
+        r#"System info:
   SDL2 [{}]
   SDL2 image [{}]
   SDL2 mixer [{}]
   SDL2 ttf [{}]"#,
-            sdl2::version::version(),
-            sdl2::image::get_linked_version(),
-            sdl2::mixer::get_linked_version(),
-            sdl2::ttf::get_linked_version())
+        sdl2::version::version(),
+        sdl2::image::get_linked_version(),
+        sdl2::mixer::get_linked_version(),
+        sdl2::ttf::get_linked_version()
+    )
 }
