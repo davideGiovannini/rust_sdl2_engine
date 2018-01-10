43af8f70d6622741dd8be020e3de7da6957b24ea
Davide Giovannini <giovannini.davide90@gmail.com>
2018-01-10 22:11:35 +0100

    Fixed bug with bitmap font rendering, added \n support

--- src/font/mod.rs
+++ src/font/mod.rs
@@ -31,28 +31,35 @@ impl BitmapFont{
         let width = self.texture.query().width;
         let width = width - width % self.char_width;
 
-        for (c, character) in text.char_indices(){
+        let mut line = 0;
+
+        // This is needed to rebalance the index when going on a new line \n
+        let mut x_counter_offset = 0;
+
+        for (char_count, character) in text.char_indices(){
+            if character == '\n'{
+                line += 1;
+                x_counter_offset = char_count + 1;
+                continue
+            }
+
             let char_index = character as usize - 32;
 
-            let x = ((char_index as u32 * self.char_width) % width) as i32;
-            let y = ((char_index as u32 * self.char_width / width) * self.char_height) as i32;
+            let src_x = ((char_index as u32 * self.char_width) % width) as i32;
+            let src_y = ((char_index as u32 * self.char_width / width) * self.char_height) as i32;
+            let src_rect = Rect::new(src_x, src_y, self.char_width, self.char_height);
 
+            let dest_x = ((char_count  - x_counter_offset) as u32 * self.char_width) as i32;
+            let dest_y = (line * self.char_height) as i32;
 
-            let rect = Rect::new(x,y, self.char_width, self.char_height);
+            let mut dest_rect = Rect::new(dest_x + point.0,
+                                               dest_y + point.1,
+                                               self.char_width,
+                                               self.char_height);
 
-            let dest_x = (c as u32 * self.char_width % width) as i32;
-            let dest_y = ((c as u32 / width) * self.char_height) as i32;
-
-            let mut dest_rect = Rect::new(dest_x, dest_y, self.char_width, self.char_height);
-            dest_rect.x += point.0;
-            dest_rect.y += point.1;
-
-            renderer.copy(&self.texture, rect, dest_rect).unwrap();
+            renderer.copy(&self.texture, src_rect, dest_rect).unwrap();
         }
     }
 
     // TODO add other functions (to print text centered or right aligned)
-
-    // TODO consider handling '\n' (just go to next line)
-
 }
